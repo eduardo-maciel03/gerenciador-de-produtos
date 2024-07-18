37686709a6fee4de6dfca170fabc974320b8dcfb
Eduardo <eduardomacielpereira@hotmail.com>
2024-07-18 13:10:31 -0300

    feat: adiciona componente no-items a lista

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -6,11 +6,12 @@ import { ProductsService } from '../../shared/services/products.service';
 import { Product } from '../../shared/interfaces/product.interface';
 import { ConfirmationDialogService } from '../../shared/services/confirmation-dialog.service';
 import { filter } from 'rxjs';
+import { NoItemsComponent } from './components/no-items/no-items.component';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [ CardComponent, RouterLink, MatButtonModule ],
+  imports: [ CardComponent, RouterLink, MatButtonModule, NoItemsComponent ],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
