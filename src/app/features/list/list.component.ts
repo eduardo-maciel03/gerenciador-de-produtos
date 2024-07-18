6eed20d54bb96d567f6a0b301477a9514169e568
Eduardo <eduardomacielpereira@hotmail.com>
2024-07-18 13:29:19 -0300

    feat: adiciona snack bar ao excluir um produto

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -7,6 +7,7 @@ import { Product } from '../../shared/interfaces/product.interface';
 import { ConfirmationDialogService } from '../../shared/services/confirmation-dialog.service';
 import { filter } from 'rxjs';
 import { NoItemsComponent } from './components/no-items/no-items.component';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-list',
@@ -21,6 +22,7 @@ export class ListComponent {
   productsService = inject(ProductsService)
   router = inject(Router);
   confirmationDialog = inject(ConfirmationDialogService);
+  matSnackBar = inject(MatSnackBar);
 
   onEdit(product: Product) {
     this.router.navigate(['/edit-product', product.id]);
@@ -36,6 +38,8 @@ export class ListComponent {
             this.productsService.getAll().subscribe((products) => {
               this.products.set(products);
             });
+
+            this.matSnackBar.open(`Produto ${product.title.toUpperCase()} excluído!`, 'Ok');
           });
       });
   }
