f9b5671d600e85f25f38197135af166f3e819d26
Eduardo <eduardomacielpereira@hotmail.com>
2024-07-18 10:05:24 -0300

    feat: configura componente de listagem

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { CardComponent } from './components/card/card.component';
+import { RouterLink } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [ CardComponent, RouterLink, MatButtonModule ],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
